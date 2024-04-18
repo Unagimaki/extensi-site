6fb81d0bb6e8166679c3c8e341d92b75f359aadb
Unagimaki <m.yark0v.2000@gmail.com>
2024-04-18 21:37:56 +0400

    team

--- src/features/Artists/Artists.tsx
+++ src/features/Artists/Artists.tsx
@@ -1,5 +1,4 @@
-import { Button } from 'components'
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 import img from '/public/images/team-image.png'
 import classes from './artists.module.scss'
 
@@ -8,19 +7,13 @@ interface ArtistsProps {
 }
 
 export const Artists: FC<ArtistsProps> = ({ artists }) => {
-  const [state, setState] = useState(false)
-
-  const handleAddArtists = () => {
-    setState(() => !state)
-  }
-
   const setClassNameForName = (index: number): string => {
     let className = classes.name;
     index === 0 || index === 5 || index === 8 ?
     (className += ' ' + classes.left_center) :
     index === 1 || index === 4 || index === 7 ?
     (className += ' ' + classes.left) :
-    index === 2 ?
+    index === 2 || index === 9 ?
     (className += ' ' + classes.right_name) :
     index === 3 || index === 6 ?
     (className += ' ' + classes.right_center) :
@@ -28,22 +21,6 @@ export const Artists: FC<ArtistsProps> = ({ artists }) => {
     return className;
   };
   
-  // const setClassNameForNumber = (index: number): string => {
-  //   let className = classes.number;
-  //   index === 0 || index === 5 || index === 8
-  //     ? (className += ' ' + classes.left)
-  //     : index === 1 || index === 6
-  //     ? (className += ' ' + classes.right)
-  //     : index === 3
-  //     ? (className += ' ' + classes.left_number)
-  //     : index === 4
-  //     ? (className += ' ' + classes.right_number)
-  //     : index === 2 || index === 7
-  //     ? (className += ' ' + classes.center_number)
-  //     : className;
-  //   return className;
-  // };
-  let filteredArtists = !state ? artists.slice(0, -2) : artists.filter((item, index) => index !== 7 && index !== 8)
     
   return (
     <div className={classes.team}>
@@ -58,22 +35,15 @@ export const Artists: FC<ArtistsProps> = ({ artists }) => {
       <div className={classes.list_container}>
         <div className={classes.list}>
           {
-            filteredArtists.map((item, index) => {
+            artists.map((item, index) => {
               return(
                 <div className={classes.item} key={index}>
-                  {/* <div className={setClassNameForNumber(index)}>
-                    {
-                      index === 0 || index === 1 || index === 2 || index === 3 ? '(co-founder)' :
-                      (state && index === 7 || state && index === 8) ? `(${index + 3})` : `(0${index + 1})`
-                    }
-                  </div> */}
                   <div className={setClassNameForName(index)}>{item}</div>
                 </div>
               )
             })
           }
         </div>
-          <Button classNames={classes.button} variant='black' onClick={handleAddArtists} />
       </div>
     </div>
   )
