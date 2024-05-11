c9f949a639db9fafb8b6b9fb62dcebed2a04d825
Unagimaki <m.yark0v.2000@gmail.com>
2024-05-11 23:39:47 +0400

    models loading fixed

--- src/pages/LandingPage/AboutSection/AboutSection.tsx
+++ src/pages/LandingPage/AboutSection/AboutSection.tsx
@@ -60,7 +60,7 @@ export const AboutSection: FC<AboutSectionProps> = ({
               <CanvasLoader
                 onChangeProgress={progress => {
                   console.log(progress);
-                  setTimeout(() => {dispatch(setModelsLoaded(true)), alert('loaded timeout')}, 9000)
+                  // setTimeout(() => {dispatch(setModelsLoaded(true)), alert('loaded timeout')}, 9000)
 
                   if (progress >= 99) {
                     console.log('loaded');
