0d53f5c89d853b2e455465d35308bd42a06e730f
Unagimaki <m.yark0v.2000@gmail.com>
2024-05-12 12:24:29 +0400

    smooth scroll added

--- src/features/Layout/Layout.tsx
+++ src/features/Layout/Layout.tsx
@@ -1,27 +1,11 @@
-import React, { ReactNode, useEffect, useState } from 'react'
+import React, { ReactNode } from 'react'
 import { Header, Footer } from 'features'
 import s from './layout.module.scss'
-import { useRouter } from 'next/router'
-import LoadingOverlay from 'components/LoadingOverlay/LoadingOverlay'
 
 interface LayoutProps {
   children: ReactNode
 }
 export const Layout = ({ children }: LayoutProps) => {
-  const { pathname } = useRouter?.()
-  // const [showLoading, setShowLoading] = useState<boolean>(true) // show loader
-
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     setShowLoading(false)
-  //   }, 2500)
-  //   return () => clearTimeout(timeout)
-  // }, [])
-
-  // if (pathname === '/' && showLoading) {
-  //   return 
-  // }
-
   return (
     <div className={s.layout}>
       <Header />
